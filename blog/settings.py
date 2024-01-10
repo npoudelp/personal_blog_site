f9e6890dd54e5888dda1e7b02e58bbb416f4b4de
npoudelp <npoudelp@gmail.com>
2024-01-10 20:02:11 +0545

    static collectable made

--- blog/settings.py
+++ blog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -131,9 +132,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
 TINYMCE_JS_URL = os.path.join(STATIC_URL, "tinymce_6.8.2/tinymce/js/tinymce/tinymce.min.js")
