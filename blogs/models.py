a674034fb640952fdba53b0dc103d44b8e8c652e
npoudelp <npoudelp@gmail.com>
2024-01-10 20:14:17 +0545

    model null true

--- blogs/models.py
+++ blogs/models.py
@@ -20,11 +20,11 @@ class post(models.Model):
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     author = models.CharField(choices=AUTHORS, max_length=25)
     title = models.CharField(max_length=255)
-    blog = mce_model.HTMLField()
+    blog = mce_model.HTMLField(null=True, blank=True)
     date = models.DateField()
     tags = models.CharField(max_length=25, default='Empty')
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     status = models.CharField(choices=STAT, max_length=25, default='draft')
 
     def __str__(self):
-        return self.title
+        return f'{self.title} -> {self.status}'
